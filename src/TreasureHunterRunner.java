6ad3c2989b91bf9de4649d3baac47a337d827faf
BT_1N3_21 <manihaa@nycstudents.net>
2025-01-14 10:29:14 -0500

    completed TH-4 Added a "Marsh" terrain

--- src/TreasureHunterRunner.java
+++ src/TreasureHunterRunner.java
@@ -1,6 +1,7 @@
 public class TreasureHunterRunner {
     public static void main(String[] args) {
         TreasureHunter game = new TreasureHunter();
+        //System.out.println(Math.random() * 6);
         game.play();
     }
 }
